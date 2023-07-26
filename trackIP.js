8f41e88fe743529c094c8b75fdd7ed5b6551a767
Islam Birouk <121451068+islamdev2022@users.noreply.github.com>
2023-07-26 16:43:39 +0100

    Add files via upload

---
+++ trackIP.js
@@ -0,0 +1,39 @@
+const btn= document.getElementById('btn')
+var map = L.map('map').setView([36.82, 5.7667], 13);
+var marker = L.marker([36.821, 5.763]).addTo(map);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '© OpenStreetMap'
+}).addTo(map);
+
+
+btn.addEventListener('click',()=>{
+const IP=document.getElementById('IP').value
+if (IP==''){
+    alert('sdsdfs')
+}else{
+    const apikey='at_0jghSUW5goMQJAp1KLJqt3IrL1yTu'
+const api=`https://geo.ipify.org/api/v2/country,city?apiKey=${apikey}&ipAddress=${IP}`
+
+fetch (api).then(Response => Response.json()).then(data=>{
+    document.getElementById('ipA').innerText=data.ip;
+    document.getElementById('loc').innerText=data.location.region
+    document.getElementById('city').innerText=", "+data.location.city;
+    document.getElementById('postcode').innerText=data.location.postalCode
+    document.getElementById('time').innerText="UTC "+data.location.timezone;
+    document.getElementById('isp').innerText=data.isp
+   
+   const lat= data.location.lat;
+   const lng = data.location.lng;
+    // Update the marker's position
+    marker.setLatLng([lat, lng]);
+
+    // Set the map view to the new marker position
+    map.setView([lat, lng], 13);
+
+}).catch(error => {
+   
+    alert('Error fetching data:', error);
+  });
+}
+})
